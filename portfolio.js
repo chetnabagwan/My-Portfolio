72490af1347c430d62c7e414b0de0a05017d6954
chetna Bagwan <80641491+chetnabagwan@users.noreply.github.com>
2023-12-16 15:13:48 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,416 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: true, 
+};
+
+const seo = {
+  title: "Chetna's Portfolio",
+  description:
+    "I am the person with problem-solving approach, dedicated to working with excellent teamwork skills and tasks always done on time. I am always ready to accept new challenges, learning opportunities and constant self improvement. I am an aspiring Software Engineer who enjoys connecting the dots:- be it ideas from different disciplines, people from different teams, or applications from different industries. I have strong technical skills and an academic background in engineering, data analytics and cloud, bringing ideas to life.",
+  og: {
+    title: "Chetna Bagwan",
+    type: "website",
+    url: "#"
+  },
+};
+
+//Home Page
+const greeting = {
+  title: "Chetna Bagwan",
+  logo_name: "ChetnaBagwan",
+  subTitle:
+  "I am the person with problem-solving approach, dedicated to working with excellent teamwork skills and tasks always done on time. I am always ready to accept new challenges, learning opportunities and constant self improvement. I am an aspiring Software Engineer who enjoys connecting the dots:- be it ideas from different disciplines, people from different teams, or applications from different industries. I have strong technical skills and an academic background in engineering, data analytics and cloud, bringing ideas to life.",
+  resumeLink:
+    "https://drive.google.com/file/d/1KjHCJM6dPCdnb0Zs7ISRc6f7PKaaIfCu/view?usp=share_link",
+  portfolio_repository: "https://drive.google.com/file/d/1KjHCJM6dPCdnb0Zs7ISRc6f7PKaaIfCu/view?usp=share_link",
+  githubProfile: "https://github.com/chetnabagwan",
+  resumeLinkChetna:"https://drive.google.com/file/d/16k8bL2x4hSpHZjMJqOPUml5-iSXjIWmH/view?pli=1",
+};
+
+const socialMediaLinks = [
+  
+  {
+    name: "Github",
+    link: "https://github.com/chetnabagwan",
+    fontAwesomeIcon: "fa-github", 
+    backgroundColor: "#181717", 
+    
+  },
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/chetna-bagwan-aa0312209/",
+    fontAwesomeIcon: "fa-linkedin-in",
+    backgroundColor: "#0077B5",
+  },
+];
+
+const skills = {
+  data: [
+    {
+      title: "Data Analytics",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Collecting and organizing data sets",
+        "⚡ Creating basic visualizations for data interpretation",
+        "⚡ Understanding fundamental data analysis techniques",
+      ],
+      softwareSkills: [
+        {
+          skillName: "Python",
+          fontAwesomeClassname: "simple-icons:python",
+          style: {
+            color: "#3776AB",
+          },
+        },
+        {
+          skillName: "R",
+          fontAwesomeClassname: "simple-icons:r",
+          style: {
+            color: "#276DC3",
+          },
+        },
+        {
+          skillName: "SQL",
+          fontAwesomeClassname: "simple-icons:sql",
+          style: {
+            color: "#F29111",
+          },
+        },
+        {
+          skillName: "Excel",
+          fontAwesomeClassname: "simple-icons:microsoftexcel",
+          style: {
+            color: "#217346",
+          },
+        },
+        {
+          skillName: "Tableau",
+          fontAwesomeClassname: "simple-icons:tableau",
+          style: {
+            color: "#E97627",
+          },
+        },
+        {
+          skillName: "Pandas",
+          fontAwesomeClassname: "simple-icons:pandas",
+          style: {
+            color: "#150458",
+          },
+        },
+        {
+          skillName: "NumPy",
+          fontAwesomeClassname: "simple-icons:numpy",
+          style: {
+            color: "#013243",
+          },
+        },
+      ],
+
+      // softwareSkills: [
+      //   {
+      //     skillName: "HTML5",
+      //     fontAwesomeClassname: "simple-icons:html5",
+      //     style: {
+      //       color: "#E34F26",
+      //     },
+      //   },
+      //   {
+      //     skillName: "CSS3",
+      //     fontAwesomeClassname: "fa-css3",
+      //     style: {
+      //       color: "#1572B6",
+      //     },
+      //   },
+        
+      //   {
+      //     skillName: "JavaScript",
+      //     fontAwesomeClassname: "simple-icons:javascript",
+      //     style: {
+      //       backgroundColor: "#000000",
+      //       color: "#F7DF1E",
+      //     },
+      //   },
+      //   {
+      //     skillName: "ReactJS",
+      //     fontAwesomeClassname: "simple-icons:react",
+      //     style: {
+      //       color: "#61DAFB",
+      //     },
+      //   },
+      //   {
+      //     skillName: "NodeJS",
+      //     fontAwesomeClassname: "simple-icons:node-dot-js",
+      //     style: {
+      //       color: "#339933",
+      //     },
+      //   },
+      //   {
+      //     skillName: "MongoDB",
+      //     fontAwesomeClassname: "simple-icons:mongodb",
+      //     style: {
+      //       color: "#47A248",
+      //     },
+      //   },
+      //   {
+      //     skillName: "NPM",
+      //     fontAwesomeClassname: "simple-icons:npm",
+      //     style: {
+      //       color: "#CB3837",
+      //     },
+      //   },
+        
+         
+      // ],
+    },
+    
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "Vellore Institute of Technology Bhopal",
+      subtitle: "B.Tech. in Computer Engineering",
+      logo_path: "vit.png",
+      alt_name: "VIT Bhopal",
+      duration: "2020 - Present",
+      descriptions: [
+        "⚡ I have studied some software engineering subjects like Algorithms, DBMS, OS, CNN,  etc.",
+        "⚡ Apart from this, I have done courses on AWS, Salesforce and Azure",
+      ],
+      website_link: "https://vitbhopal.ac.in/",
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+
+    {
+      title: "AWS Certified Cloud Practitioner",
+      subtitle: "AWS",
+      logo_path: "aws-logo.jfif",
+      certificate_link:
+        "https://drive.google.com/file/d/1_HLonHeSzC9Cd7kZAjQoTvqgLxaFAULN/view",
+    },
+    {
+      title: "Salesforce Developer",
+      subtitle: "SmartInternz",
+      logo_path: "salesforce.png",
+      certificate_link:
+        "https://drive.google.com/file/d/1tTJIHtVliVlaC2_3gg9IwGzngb3kNmG_/view",
+    },
+    {
+      title: "Microsoft Certified: Azure Fundamentals",
+      subtitle: "Microsoft",
+      logo_path: "azure.png",
+      certificate_link:
+        "https://drive.google.com/file/d/12e98kvWMQfJUHKebYLLOJhRA0zmukPYl/view",
+    },
+
+    // {
+    //   title: "Global Entrepreneurship Summit",
+    //   subtitle: "E Cell IIT Kharagpur ",
+    //   logo_path: "ecell-iit.png",
+    //   certificate_link:
+    //     "https://drive.google.com/file/d/1gftVH9tPwAd-V_gBZiQCBkpHbve-xgDe/view?usp=share_link",
+    //   alt_name: "E cell",
+    //   color_code: "#0C9D5899",
+    // },
+    // {
+    //   title: "Data Science",
+    //   subtitle: "- Alex Aklson",
+    //   logo_path: "ibm_logo.png",
+    //   certificate_link:
+    //     "https://www.coursera.org/account/accomplishments/specialization/PLEAPCSJBZT5",
+    //   alt_name: "IBM",
+    //   color_code: "#1F70C199",
+    // },
+    // {
+    //   title: "Big Data",
+    //   subtitle: "- Kim Akers",
+    //   logo_path: "microsoft_logo.png",
+    //   certificate_link:
+    //     "https://drive.google.com/file/d/164zKCFOsI4vGqokc-Qj-e_D00kLDHIrG/view",
+    //   alt_name: "Microsoft",
+    //   color_code: "#D83B0199",
+    // },
+    // {
+    //   title: "Advanced Data Science",
+    //   subtitle: "- Romeo Kienzler",
+    //   logo_path: "ibm_logo.png",
+    //   certificate_link:
+    //     "https://www.coursera.org/account/accomplishments/verify/BH2T9BRU87BH",
+    //   alt_name: "IBM",
+    //   color_code: "#1F70C199",
+    // },
+
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Work, Internship and Volunteership",
+  description:
+    "I am currenly doing internship at WatchGuard Technologies, Noida. My role is of a Software Developer. I also love organising and managing events and that is why I am involved in club activies.",
+  header_image_path: "experience.svg",
+  sections: [
+     {
+      title: "Internships",
+      experiences: [
+        {
+          title: "Software Developer Intern",
+          company: "WatchGuard Technologies",
+          company_url: "https://www.watchguard.com/",
+          logo_path: "wg-logo.png",
+          duration: "September 2023 - Present",
+          location: "Noida, Uttar Pradesh",
+          description:
+          
+            "Utilized Python along with pytest and unittest for robust testing, incorporating cybersecurity principles throughout. Spearheaded the development of task manager and hotel management system projects, showcasing proficiency in these domains.",
+            
+            color: "#ee3c26",
+        },
+      ],
+    },
+
+    // {
+    //   title: "Hackathons",
+    //   experiences: [
+    //     {
+    //       title: "Enosium",
+    //       company: "IIT Indore",
+    //       company_url: "https://drive.google.com/file/d/1PVKAvrZHXPmj-epWjyH7L_YUljgwqc4y/view?usp=share_link",
+    //       logo_path: "pnapo.png",
+    //       location: "online",
+    //       description:
+    //         "Our team had scored 2 rank in Enosium hackathon organised by IIT Indore.",
+    //       color: "#ee3c24",
+    //     },
+
+    //     {
+    //       title: "Coding Velocity",
+    //       company: "Pnapaao Private Limited",
+    //       company_url: "https://drive.google.com/file/d/1PVKAvrZHXPmj-epWjyH7L_YUljgwqc4y/view?usp=share_link",
+    //       logo_path: "pnapo.png",
+    //       location: "online",
+    //       description:
+    //         "Our team had scored 39 global rank in Coding Velocity organised by Pnapaao Private Limited. We won under a category of top 50 teams to received a course on stock market training",
+    //       color: "#ee3c26",
+    //     },
+    //     {
+    //       title: "Weave The Webs",
+    //       company: "Padre Conceicao College of Engineering",
+    //       company_url:
+    //         "#",
+    //       logo_path: "padre.webp",
+    //       location: "online",
+    //       description:
+    //       "Weave The Webs hackathon hosted by Padre Conceicao College of Engineering, oum team provided the best solution of given problem statement. our team created a full functionality organic D2C ecommerce website with MERN stack. This particular hackathon was won by our team, which finished in third place and was awarded a prize of 2,000.",
+    //       color: "#0071C5",
+    //     },
+    //   ],
+    // },
+    {
+      title: "Leadership",
+      experiences: [
+        {
+          title: "Core Member",
+          company: "Mozilla Firefox club VIT Bhopal",
+          company_url: "https://about.google/",
+          logo_path: "mfc.png",
+          duration: "November 2021 - November 2022",
+          location: "Bhopal, Madhya Pradesh",
+          description:
+            "Compliance with the club President’s performance requirements and instructions.Managed the core team",
+          color: "#4285F4",
+        }
+
+      ],
+    },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects makes use of vast variety of latest technology tools. My best experience is to create Hotel Booking System and Task Manager Applications which I built during my internship at WatchGuard.",
+  avatar_image_path: "projects_image.svg",
+};
+
+
+
+// const publications = {
+//   data: [
+//     {
+//       id: "MDEwOlJlcG9zaXRvcnkyNDU0NjcyNzQ=",
+//       name: "Loan Eligiblity Predictor",
+//       description: 
+//       "A smart individual would diligently measure the amount and purpose of each loan against their own specific information to determine their ability to pay the loan back. There is certainly a large scope for programs and algorithms that can help in making this decision with high accuracy. ",
+//       url:
+//         "https://ailoec-technomates-ztgx.onrender.com",
+//     },
+//     {
+//       id: "MDEwOlJlcG9zaXRvcnkyNDU0NjcyNzi=",
+//       name: "Ecommerce website",
+//       description:
+//       "An ecommerce store built with MERN stack, and utilizes third party API's. This ecommerce store enable two main different flows or implementations:1. Buyers browse the store categories, products and brands 2. Admins manage and control the entire store components",
+//       url:
+//       "#"
+//     },
+//     {
+//       id: "MDEwOlJlcG9zaXRvcnkyNDU0NjcyNze=",
+//       name: "The New Era of Computers",
+//       description: 
+//       "Gestures captured with a built-in camera and processed with color segmentation & detection technique. The user will be allowed to control some of the computer cursor functions with their hands which bear colored caps on fingertips. A user can perform left clicks, right clicks, and double clicks, scrolling up or down using their hand in different gestures.",
+//       url:
+//         "https://github.com/chetnabagwan/The_new_era-of_computers",
+//     },
+    
+//   ],
+// };
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "chetna-profile.png",
+    description:
+      "I am available on these social media. You can message me, I will reply within 24 hours. I can help you with Data Analytics and Machine Learning projects.",
+  },
+  // blogSection: {
+  //   title: "Blogs",
+  //   subtitle:
+  //     "For individual fundamental empowerment, I like to write powerful lessons that create impact on each of the reader individually to change the core of their character.",
+  //   link: "https://ashutoshhathidara.wordpress.com",
+  //   avatar_image_path: "blogs_image.svg",
+  // },
+  addressSection: {
+    title: "Address",
+    subtitle:
+      "Purana Thana Road,Ashta Dist. Sehore  466116",
+    avatar_image_path: "address_image.svg",
+    location_map_link: "https://goo.gl/maps/SFtpLWjm8v7M4BoE9",
+  },
+  phoneSection: {
+    title: "Phone Number",
+    subtitle: "+91 7389430182",
+  },
+};
+
+export {
+  settings,
+  seo,
+  greeting,
+   socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+};
